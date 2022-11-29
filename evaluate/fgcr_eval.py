cdb65ac36f5d3b84a01efb5c6dab06e33018b0b0
Italo Silva <italo@maleldil.com>
2022-11-29 16:28:19 +0000

    Update eval script with ACE support

--- evaluate/fgcr_eval.py
+++ evaluate/fgcr_eval.py
@@ -1,13 +1,31 @@
+from __future__ import annotations
+import argparse
+
+from dataclasses import dataclass
+from pathlib import Path
+
 from sklearn import metrics
-from typing import NamedTuple
 
 
-class Metric(NamedTuple):
+@dataclass
+class Metric:
+    name: str
+    precision: float
+    recall: float
     f1: float
     em: float
 
+    def __str__(self) -> str:
+        return (
+            f"{self.name}\n"
+            f"Precision: {self.precision:.2%}\n"
+            f"Recall:    {self.recall:.2%}\n"
+            f"F1:        {self.f1:.2%}\n"
+            f"EM:        {self.em:.2%}"
+        )
 
-def evaluate(gold: list[list[str]], predicted: list[list[str]]) -> Metric:
+
+def evaluate(gold: list[list[str]], predicted: list[list[str]], name: str) -> Metric:
     """Evaluate labels for F1 and Exact Match
 
     Args:
@@ -16,7 +34,7 @@ def evaluate(gold: list[list[str]], predicted: list[list[str]]) -> Metric:
            predicted instances, each a list of predicted labels
 
     Returns:
-        Metric: tuple of (F1, EM)
+        Metric: set of metrics we track
     """
     exact_match = 0
     y_gold: list[str] = []
@@ -27,6 +45,65 @@ def evaluate(gold: list[list[str]], predicted: list[list[str]]) -> Metric:
         y_gold.extend(g)
         y_predicted.extend(p)
 
-    f1 = metrics.f1_score(y_gold, y_predicted, average="macro")
+    p, r, f1, _ = metrics.precision_recall_fscore_support(
+        y_gold, y_predicted, average="macro"
+    )
     em = exact_match / len(gold)
-    return Metric(float(f1), em)
+
+    return Metric(
+        name=name,
+        precision=float(p),
+        recall=float(r),
+        f1=float(f1),
+        em=em,
+    )
+
+
+@dataclass
+class Entry:
+    token: str
+    gold: str
+    pred: str
+    prob: float
+
+
+def evaluate_ace(path: Path) -> None:
+    sentences: list[list[Entry]] = []
+    sentence: list[Entry] = []
+    with open(path) as f:
+        for line in f:
+            if not line.strip():
+                sentences.append(sentence)
+                sentence = []
+            else:
+                token, gold, pred, prob = line.split()
+                sentence.append(Entry(token, gold, pred, float(prob)))
+
+    golds = [[e.gold for e in sent] for sent in sentences]
+    preds = [[e.pred for e in sent] for sent in sentences]
+    result = evaluate(golds, preds, "BERT - Test")
+    print(result)
+
+
+evals = {
+    "ace": evaluate_ace,
+}
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("model", help=f"Model to evaluate. One of {list(evals)}.")
+    argparser.add_argument("path", help="Path to predictions file or folder.")
+
+    args = argparser.parse_args()
+    model = args.model.lower().strip()
+    path = Path(args.path)
+
+    if model not in evals:
+        raise ValueError("Invalid model: " + args.model)
+
+    print(evals[model](path))
+
+
+if __name__ == "__main__":
+    main()
